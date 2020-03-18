d6fdcd6f44f767299fad7294bf0e747e5d84ae6a
pierre <nanashi4781@gmail.com>
2020-03-18 16:09:55 +0100

    panneaux

--- src/Controller/PanneauController.php
+++ src/Controller/PanneauController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\City;
 use App\Entity\Ride;
+use App\Entity\Company;
+use App\Entity\Users;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,18 +24,27 @@ class PanneauController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
 
             $schedule = $request->request->get('schedule');
-            $arrival = $request->request->get('arrival');
+            $arrival = $request->request->get('arrival', null);
+            $departure = $request->request->get('departure', null);
             $spaceAvailable = $request->request->get('spaceAvailable');
             $observations = $request->request->get('observations');
 
+            $idCity = !is_null($arrival) ? $arrival : $departure;
+
             $city = $this->getDoctrine()
                 ->getRepository(City::class)
-                ->find($arrival);
+                ->find($idCity);
+
 
             $createRide = new Ride();
 
             $createRide->setSchedule(\DateTime::createFromFormat('H:i', $schedule));
-            $createRide->setArrival($city);
+
+            if (!is_null($arrival)){
+                $createRide->setArrival($city);
+            }else{
+                $createRide->setDeparture($city);
+            }
             $createRide->setUser($this->getUser());
             $createRide->setSpaceAvailable($spaceAvailable);
             $createRide->setObservations($observations);
@@ -43,14 +54,18 @@ class PanneauController extends AbstractController
             $entityManager->flush();
         }
 
-        $ville = $this->getDoctrine()
+        $ville = $this->getDoctrine() //afficher les villes ds la recherche//
         ->getRepository(City::class)
         ->findAll();
 
-
+        $user = $this->getUser(); //je recupere le user connecté//
+        $adress = $user->getAdress(); //je recupere son adresse//
+        $name = $adress->getCity();// je recupere la ville (city)//
+        $company = $user->getCompany();
 
         return $this->render('panneau/index.html.twig', [
             'ville' => $ville,
+            'adress'=> $adress,
             'controller_name' => 'PanneauController',
         ]);
     }
